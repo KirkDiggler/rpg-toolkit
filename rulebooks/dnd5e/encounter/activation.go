4df273350694ccebc8d67274ecce782f9e913318
fadedpez <18429141+fadedpez@users.noreply.github.com>
2026-09-18 22:38:17 -0400

    feat(encounter): record activation concentration outcomes

--- rulebooks/dnd5e/encounter/activation.go
+++ rulebooks/dnd5e/encounter/activation.go
@@ -401,6 +401,17 @@ func (e *Encounter) prepareActivation(in *RecordActivationInput) ([]preparedActi
 		prepared = append(prepared, preparedActivationBeat{payload: savedBytes, subjects: subjects})
 	}
 
+	checks, err := e.prepareConcentrationChecks("record activation", in.Actor, in.ConcentrationChecks)
+	if err != nil {
+		return nil, err
+	}
+	breaks, err := e.prepareConcentrationBreaks("record activation", in.Actor, in.ConcentrationBreaks)
+	if err != nil {
+		return nil, err
+	}
+	prepared = append(prepared, checks...)
+	prepared = append(prepared, breaks...)
+
 	for i, result := range in.Results {
 		resultPayload, validationErr := e.prepareActivationResult("record activation", i, result)
 		if validationErr != nil {
